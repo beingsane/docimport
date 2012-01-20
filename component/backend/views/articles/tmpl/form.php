3ebae7209020d8ac60d7a60a0b7b5876d681feb0
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2012-01-20 23:37:39 +0200

    Regression in FOF

---
+++ component/backend/views/articles/tmpl/form.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *  @package DocImport
+ *  @copyright Copyright (c)2010-2012 Nicholas K. Dionysopoulos
+ *  @license GNU General Public License version 3, or later
+ */
+
+defined('_JEXEC') or die();
+
+echo $this->loadAnyTemplate('admin:com_docimport/article/form');
