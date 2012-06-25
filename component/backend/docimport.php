9a36df360720244030ea9c2ef5feac7289295b09
tampe125 <tampe125@gmail.com>
2012-06-25 08:48:45 +0200

    ! Docimport will load FOF from Joomla library folder ! Fixed load of admonition images (absolute path instead of relative one) ! Checkbox on articles view in backend will load the correct id

--- component/backend/docimport.php
+++ component/backend/docimport.php
@@ -12,7 +12,7 @@ defined('_JEXEC') or die();
 //require_once JPATH_COMPONENT_ADMINISTRATOR.'/version.php';
 
 // Include FOF
-include_once JPATH_ADMINISTRATOR.'/components/com_docimport/fof/include.php';
+include_once JPATH_ROOT.'/libraries/fof/include.php';
 
 // Dispatch
 FOFDispatcher::getAnInstance('com_docimport')->dispatch();
