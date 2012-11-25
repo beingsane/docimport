4f9359ec423e23a98c0f6f8bf661b6b5bcb26632
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2012-11-25 18:50:55 +0200

    Add AJAX reordering support to categories and articles view

--- component/backend/views/categories/tmpl/default.php
+++ component/backend/views/categories/tmpl/default.php
@@ -7,10 +7,12 @@
 
 defined('_JEXEC') or die();
 
-$this->loadHelper('Select');
-$this->loadHelper('Format');
+$this->loadHelper('select');
+$this->loadHelper('format');
 
 JHtml::_('behavior.tooltip');
+
+$hasAjaxOrderingSupport = $this->hasAjaxOrderingSupport();
 ?>
 <form action="index.php" method="post" name="adminForm" id="adminForm">
 <input type="hidden" name="option" value="com_docimport" />
@@ -22,9 +24,14 @@ JHtml::_('behavior.tooltip');
 <input type="hidden" name="filter_order_Dir" id="filter_order_Dir" value="<?php echo $this->lists->order_Dir ?>" />
 <input type="hidden" name="<?php echo JFactory::getSession()->getToken();?>" value="1" />
 
-<table class="table table-striped" width="100%">
+<table class="table table-striped" width="100%" id="itemsList">
 	<thead>
 		<tr>
+			<?php if($hasAjaxOrderingSupport !== false): ?>
+			<th width="20px">
+				<?php echo JHtml::_('grid.sort', '<i class="icon-menu-2"></i>', 'ordering', $this->lists->order_Dir, $this->lists->order, null, 'asc', 'JGRID_HEADING_ORDERING'); ?>
+			</th>
+			<?php endif; ?>
 			<th width="60">
 				<?php echo JHTML::_('grid.sort', 'Num', 'docimport_category_id', $this->lists->order_Dir, $this->lists->order, 'browse') ?>
 			</th>
@@ -38,10 +45,12 @@ JHtml::_('behavior.tooltip');
 			<th width="10%">
 				<?php echo JHTML::_('grid.sort', 'COM_DOCIMPORT_CATEGORIES_FIELD_SLUG', 'slug', $this->lists->order_Dir, $this->lists->order, 'browse') ?>
 			</th>
+			<?php if($hasAjaxOrderingSupport === false): ?>
 			<th width="8%">
 				<?php echo JHTML::_('grid.sort', 'COM_DOCIMPORT_COMMON_FIELD_ORDERING', 'ordering', $this->lists->order_Dir, $this->lists->order, 'browse') ?>
 				<?php echo JHTML::_('grid.order', $this->items); ?>
 			</th>
+			<?php endif; ?>
 			<th width="8%">
 				<?php echo JHTML::_('grid.sort', 'COM_DOCIMPORT_COMMON_FIELD_ENABLED', 'enabled', $this->lists->order_Dir, $this->lists->order, 'browse') ?>
 			</th>
@@ -52,8 +61,10 @@ JHtml::_('behavior.tooltip');
 			<?php endif; ?>
 		</tr>
 		<tr>
+			<?php if($hasAjaxOrderingSupport !== false): ?>
 			<td></td>
-			<td>
+			<?php endif; ?>
+			<td colspan="2">
 				<input type="checkbox" name="toggle" value="" onclick="Joomla.checkAll(this);" />
 			</td>
 			<td class="form-inline">
@@ -70,7 +81,9 @@ JHtml::_('behavior.tooltip');
 				</nobr>
 			</td>
 			<td></td>
+			<?php if($hasAjaxOrderingSupport === false): ?>
 			<td></td>
+			<?php endif; ?>
 			<td></td>
 			<td>
 				<?php echo DocimportHelperSelect::published($this->getModel()->getState('enabled',''), 'enabled', array('onchange'=>'this.form.submit();', 'class' => 'input-small')) ?>
@@ -100,6 +113,27 @@ JHtml::_('behavior.tooltip');
 			$item->published = $item->enabled;
 		?>
 		<tr class="<?php echo 'row'.$m; ?>">
+			<?php if($hasAjaxOrderingSupport !== false): ?>
+			<td class="order nowrap center hidden-phone">
+			<?php if ($this->perms->editstate) :
+				$disableClassName = '';
+				$disabledLabel	  = '';
+				if (!$hasAjaxOrderingSupport['saveOrder']) :
+					$disabledLabel    = JText::_('JORDERINGDISABLED');
+					$disableClassName = 'inactive tip-top';
+				endif; ?>
+				<span class="sortable-handler <?php echo $disableClassName?>" title="<?php echo $disabledLabel?>" rel="tooltip">
+					<i class="icon-menu"></i>
+				</span>
+				<input type="text" style="display:none"  name="order[]" size="5"
+					value="<?php echo $item->ordering;?>" class="input-mini text-area-order " />
+			<?php else : ?>
+				<span class="sortable-handler inactive" >
+					<i class="icon-menu"></i>
+				</span>
+			<?php endif; ?>
+			</td>
+			<?php endif; ?>
 			<td align="center">
 				<?php echo $item->docimport_category_id; ?>
 			</td>
@@ -134,12 +168,14 @@ JHtml::_('behavior.tooltip');
 			<td>
 				<?php echo $this->escape($item->slug) ?>
 			</td>
+			<?php if($hasAjaxOrderingSupport === false): ?>
 			<td class="order" align="center">
 				<span><?php echo $this->pagination->orderUpIcon( $i, true, 'orderup', 'Move Up', $ordering ); ?></span>
 				<span><?php echo $this->pagination->orderDownIcon( $i, $count, true, 'orderdown', 'Move Down', $ordering ); ?></span>
 				<?php $disabled = $ordering ?  '' : 'disabled="disabled"'; ?>
 				<input type="text" name="order[]" size="5" value="<?php echo $item->ordering;?>" <?php echo $disabled ?> class="text_area" style="text-align: center" />
 			</td>
+			<?php endif; ?>
 			<td align="center">
 				<?php echo JHTML::_('grid.published', $item, $i); ?>
 			</td>
