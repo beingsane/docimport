95b97c7c14e8207bc48549ee3fa43880fd5e6c0e
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2012-11-25 18:26:13 +0200

    Bootstrapped article view

--- component/backend/views/article/tmpl/form.php
+++ component/backend/views/article/tmpl/form.php
@@ -11,45 +11,56 @@ $this->loadHelper('Select');
 
 $editor = JFactory::getEditor();
 ?>
-<form action="index.php" method="post" name="adminForm" id="adminForm">
+<form action="index.php" method="post" name="adminForm" id="adminForm" class="form-horizontal">
 	<input type="hidden" name="option" value="com_docimport" />
 	<input type="hidden" name="view" value="article" />
 	<input type="hidden" name="task" value="" />
 	<input type="hidden" name="docimport_article_id" value="<?php echo $this->item->docimport_article_id ?>" />
 	<input type="hidden" name="<?php echo JFactory::getSession()->getToken();?>" value="1" />
 	
-	<fieldset id="article-basic">
-		<legend><?php echo JText::_('COM_DOCIMPORT_ARTICLES_BASIC_TITLE'); ?></legend>
-		
-		<label for="title_field" class="main title"><?php echo JText::_('COM_DOCIMPORT_ARTICLES_FIELD_TITLE'); ?></label>
-		<input type="text" size="40" id="title_field" name="title" class="title" value="<?php echo $this->escape($this->item->title) ?>" />
-		<div class="docimport-clear"></div>
-		
-		<label for="slug_field" class="main"><?php echo JText::_('COM_DOCIMPORT_CATEGORIES_FIELD_SLUG'); ?></label>
-		<input type="text" size="30" id="slug_field" name="slug" value="<?php echo $this->escape($this->item->slug) ?>" />
-		<div class="docimport-clear"></div>
-		
-		<label for="category" class="main"><?php echo JText::_('COM_DOCIMPORT_ARTICLES_FIELD_CATEGORY'); ?></label>
-		<?php echo DocimportHelperSelect::categories($this->item->docimport_category_id, 'category') ?>
-		<div class="docimport-clear"></div>
-		
-		<label for="enabled" class="main" class="mainlabel">
+	<h3><?php echo JText::_('COM_DOCIMPORT_ARTICLES_BASIC_TITLE'); ?></h3>
+	
+	<div class="control-group">
+		<label for="title_field" class="control-label">
+			<?php echo JText::_('COM_DOCIMPORT_ARTICLES_FIELD_TITLE'); ?>
+		</label>
+		<div class="controls">
+			<input type="text" size="40" id="title_field" name="title" class="input-xxlarge" value="<?php echo $this->escape($this->item->title) ?>" />
+		</div>
+	</div>
+	
+	<div class="control-group">
+		<label for="slug_field" class="control-label">
+			<?php echo JText::_('COM_DOCIMPORT_CATEGORIES_FIELD_SLUG'); ?>
+		</label>
+		<div class="controls">
+			<input type="text" size="30" id="slug_field" name="slug" value="<?php echo $this->escape($this->item->slug) ?>" class="input-xxlarge" />
+		</div>
+	</div>
+	
+	<div class="control-group">
+		<label for="category" class="control-label">
+			<?php echo JText::_('COM_DOCIMPORT_ARTICLES_FIELD_CATEGORY'); ?>
+		</label>
+		<div class="controls">
+			<?php echo DocimportHelperSelect::categories($this->item->docimport_category_id, 'category') ?>
+		</div>
+	</div>
+	
+	<div class="control-group">
+		<label for="enabled" class="control-label">
 			<?php if(version_compare(JVERSION,'1.6.0','ge')): ?>
 			<?php echo JText::_('JPUBLISHED'); ?>
 			<?php else: ?>
 			<?php echo JText::_('PUBLISHED'); ?>
 			<?php endif; ?>
 		</label>
-		<span class="akeebasubs-booleangroup">
+		<div class="controls">
 			<?php echo JHTML::_('select.booleanlist', 'enabled', null, $this->item->enabled); ?>
-		</span>
-		<div class="akeebasubs-clear"></div>
-		
-	</fieldset>
+		</div>
+	</div>
 	
-	<fieldset id="article-description">
-		<legend><?php echo JText::_('COM_DOCIMPORT_ARTICLES_FIELD_FULLTEXT');?></legend>
-		
-		<?php echo $editor->display( 'fulltext',  $this->item->fulltext, '80%', '500', '50', '10', false ) ; ?>
-	</fieldset>
+	<h3><?php echo JText::_('COM_DOCIMPORT_ARTICLES_FIELD_FULLTEXT');?></h3>
+
+	<?php echo $editor->display( 'fulltext',  $this->item->fulltext, '100%', '600', '50', '10', false ) ; ?>
 </form>
