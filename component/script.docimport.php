4ee6de8b70cacb92ca6aef66ba8dab1e084d3f3e
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-05-22 17:45:36 +0300

    # [LOW] Pre-installation script would run the wrong SQL files on upgrade (normally not causing a problem, but still it's not the intended behaviour)

--- component/script.docimport.php
+++ component/script.docimport.php
@@ -1121,7 +1121,7 @@ class Com_DocimportInstallerScript
 		}
 
 		// Get the name of the sql file to process
-		$sqlfile = $parent->getParent()->getPath('extension_root') . '/sql/install/' . $dbDriver . '/install.sql';
+		$sqlfile = $parent->getParent()->getPath('source') . '/backend/sql/install/' . $dbDriver . '/install.sql';
 		if (file_exists($sqlfile))
 		{
 			$buffer = file_get_contents($sqlfile);
@@ -1159,7 +1159,7 @@ class Com_DocimportInstallerScript
 
 		// Update #__schemas to the latest version. Again, since I don't have
 		// access to the manifest I have to improvise...
-		$path = $parent->getParent()->getPath('extension_root') . '/sql/update/' . $dbDriver;
+		$path = $parent->getParent()->getPath('source') . '/backend/sql/update/' . $dbDriver;
 		$files = str_replace('.sql', '', JFolder::files($path, '\.sql$'));
 		if(count($files) > 0)
 		{
