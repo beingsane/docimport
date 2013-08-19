0b00b9ffa378626c4f2d41ed259ae3e08713674b
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2013-08-19 22:43:17 +0300

    Clear FOF cache on update wasn't really working

--- component/script.docimport.php
+++ component/script.docimport.php
@@ -181,11 +181,11 @@ class Com_DocimportInstallerScript
 		if (!defined('FOF_INCLUDED'))
 		{
 			@include_once JPATH_LIBRARIES . '/fof/include.php';
+		}
 
-			if (defined('FOF_INCLUDED'))
-			{
-				FOFPlatform::getInstance()->clearCache();
-			}
+		if (defined('FOF_INCLUDED'))
+		{
+			FOFPlatform::getInstance()->clearCache();
 		}
 	}
 
