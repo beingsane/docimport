ec40e72f8b777974c11b2ab7b1fb15a5f0685aee
Davide Tampellini <tampe125@gmail.com>
2016-07-01 15:17:41 +0200

    Added TOC module

--- modules/site/mod_docimport_toc/mod_docimport_toc.php
+++ modules/site/mod_docimport_toc/mod_docimport_toc.php
@@ -25,3 +25,37 @@ if (!defined('FOF30_INCLUDED') && !@include_once(JPATH_LIBRARIES . '/fof30/inclu
 {
 	throw new RuntimeException('FOF 3.0 is not installed', 500);
 }
+
+$category = $params->get('catid', 0);
+
+if (!$category)
+{
+	return;
+}
+
+$container = FOF30\Container\Container::getInstance('com_docimport');
+
+/** @var \Akeeba\DocImport\Site\Model\Articles $article */
+$article = $container->factory->model('Articles')->tmpInstance();
+$article->category($category);
+
+try
+{
+	$article->findOrFail(array('slug' => 'index'));
+}
+catch (\Exception $e)
+{
+	return;
+}
+
+// Since the TOC has a fixed HTML structure, I can use a strpos instead of parsing the whole HTML text
+// this will be much faster than creating a DOMobject and trying to traverse it
+$start = strpos($article->fulltext, '<dl class="toc">');
+$stop  = strrpos($article->fulltext, '</dl>') + 5;
+$toc   = substr($article->fulltext, $start, $stop - $start);
+
+$container->template->addCSS('site://modules/mod_docimport_toc/assets/mod_docimport_toc.css', $container->mediaVersion);
+$container->template->addCSS('media://com_docimport/css/frontend.css', $container->mediaVersion);
+
+// Load the layout file
+require_once JModuleHelper::getLayoutPath($module->module);
